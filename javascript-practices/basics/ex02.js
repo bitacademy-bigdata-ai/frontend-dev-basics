47b24fe4bd1d9154955bcba4f136a8d902101118
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-12-15 11:57:27 +0900

    Browser Programming #1

--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -27,6 +27,21 @@
     3. var f = new Function(.....)
 */
 
+/*
+자바스크립트 객체를 분류하는 또 다른 방법
+1. 내장 객체:
+   - JavaScript Engine 내부에 미리 내장되어 있는 객체
+   - Number, Boolean, Date, RegExp, Array, Object, ... : 생성자 함수
+   - parseInt, setTimeout, setInterval, .... : 일반 함수
+   	
+2. 호스트 객체:
+   - JavaScript Engine이 embeded 되어 있는 환경에 있는 객체
+   - Browser가 호스트인 경우: screen-*, location*, navigator*, DOM***, XmlHttpRequest***, ....
+   - Node가 호스트인 경우: http, os, fs, path, process, ....
+   
+3. 사용자 객체
+   - 자바스크립트 엔진이 실행되면서 생성되는 객체	    	
+*/
 console.log("===기본타입(undefined, number, string, boolean)=======");
 
 var u = undefined; // var u; 동일, 선언 정의가 구분이 없다.
